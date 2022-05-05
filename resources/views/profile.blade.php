fe7e6a73cbedf2bf72c57ef7c622aef35cfa4cc9
Waran Singh <72307375+wsd15@users.noreply.github.com>
2022-05-05 17:07:10 +0700

    progress

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,7 +10,7 @@
 
         <div class="container rounded bg-white mt-5 mb-5"> 
 
-            <div class="col-md-5 border-right"> 
+            <div class=" border-right"> 
                 <div class="p-3 py-5"> 
                     <div class="d-flex justify-content-between align-items-center mb-3"> 
                         <h4 class="text-right">Profile Settings</h4> 
@@ -36,7 +36,7 @@
                                 </div>
                             
                                
-                                <p class="text-info text-center small">Note: Selected image will not be uploaded anywhere.  It's just for demonstration purposes.</p>
+                                
                             </div>
 
                     <div class="row mt-2"> 
@@ -72,7 +72,9 @@
                             </div>
                         </div> 
                         <div class="mt-5 text-center">
-                                <button class="btn btn-primary profile-button" type="button">Save Profile</button>
+                            <x-button class="">
+                                test
+                            </x-button>
                             </div> 
                         </div> 
                     </div> 
