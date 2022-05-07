768b2a67519b8b7d10da39cd21b0e51f550089dc
RifaJames <mrifajames@gmail.com>
2022-05-07 17:45:57 +0700

    masih kurang hati/love

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -40,31 +40,31 @@
 
 
                             <div class="col-md-6"><label class="labels">First Name</label>
-                                <input type="text" class="form-control" placeholder="first name" value="">
+                                <x-input type="text" class="form-control" placeholder="first name" value=""/>
                             </div> 
                             <div class="col-md-6"><label class="labels">Last Name</label>
-                                <input type="text" class="form-control" value="" placeholder="last name">
+                                <x-input type="text" class="form-control" value="" placeholder="last name"/>
                             </div> 
                             </div> 
                             <div class="row mt-4"> 
                                 <div class="col-md-12">
                                 <label class="labels">Email</label>
-                                <input type="text" class="form-control" placeholder="enter email id" value="">
+                                <x-input type="text" class="form-control" placeholder="enter email id" value=""/>
                             </div>
                             <div class="col-md-12 mt-4"><label class="labels">Phone Number</label>
-                                <input type="text" class="form-control" placeholder="enter phone number" value="">
+                                <x-input type="text" class="form-control" placeholder="enter phone number" value=""/>
                             </div> 
                             
                             <form class="mt-4">
                                 <label for="birthday">Birthday:</label>
-                                <input type="date" id="birthday" name="birthday">
+                                <x-input type="date" id="birthday" name="birthday"/>
                             </form>
 
 
                               <div class="mt-4">
                                 <label for="pass">Password (8 characters minimum):</label>
-                                <input type="password" id="pass" name="password"
-                                       minlength="8" required>
+                                <x-input type="password" id="pass" name="password"
+                                       minlength="8" required/>
                             </div>
                         </div> 
                         <div class="mt-5 text-center">
--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -4,9 +4,7 @@
 
 
 <html>
-
-
-    
+   
 
     <div class="container d-flex justify-content-center mt-50 mb-50">
             
@@ -15,37 +13,45 @@
             
                 <div class="card card-body">
                             <div class="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
-                                <div class="mr-2 mb-3 mb-lg-0">
-                                    
-                                        <img src="https://i.imgur.com/5Aqgz7o.jpg" width="150" height="150" alt="">
-                                   
+
+                                <div class="container border">
+                                    <div class="row">
+                                        <div class="col border col-2">
+                                            <div class="mb-3 mb-lg-0">                                 
+                                                <img src="https://source.unsplash.com/random/150x150?person" width="150" height="150" alt="">
+                                        </div>
+                                        </div>
+                                        <div class="col border col-10">
+                                            <div class="mt-2 row">
+                                                <div class="col">
+                                                    <h6 class="media-title font-weight-semibold text-start" style="font-size:2vw">
+                                                        <b href="#" data-abc="true">Lord of The Flies</b>
+                                                    </h6>
+                
+                                                    <h5 class="text-start">
+                                                        <a>William Golding</a>
+                                                    </h5>
+                
+                                                    <h5 class="text-start" style="font-size:2vw">
+                                                        <b>Perpustakaan Cemerlang</b>
+                                                    </h5>
+                                                </div>
+                                                <div class="col">
+                                                    <div class="mt-2 text-end">
+                                                        <x-button class="">
+                                                            Add to Cart
+                                                        </x-button>
+                                                        </div> 
+                                                    </div> 
+                                                                                            {{-- buat characternya jadi limit --}}
+                                        <span class="limit_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur egestas, elit sit amet suscipit ultrices, justo velit sollicitudin nibh, et semper mi libero vitae turpis. Integer ornare scelerisque magna, et eleifend metus lobortis nec. Pellentesque turpis sapien, volutpat sit amet nibh eget, consequat pharetra mi. Ut luctus feugiat pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum accumsan nisl nec diam imperdiet, eu vehicula nibh pharetra. Nunc egestas lectus nunc, sed rhoncus leo egestas in. Maecenas massa augue, laoreet a elit id, consequat consectetur diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla posuere, massa eu fringilla suscipit, neque urna vulputate libero, non lobortis mauris dui non augue. Etiam vitae commodo orci, id gravida quam. Donec maximus molestie turpis sed dapibus. Nam sit amet porta dui, eget aliquet lorem. Nunc luctus ligula eget finibus placerat. Sed neque risus, ultricies sed neque faucibus, consectetur feugiat tortor.</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-
-                                <div class="media-body">
-                                    <h6 class="media-title font-weight-semibold" style="font-size:2vw">
-                                        <b href="#" data-abc="true">Lord of The Flies</b>
-                                    </h6>
-
-                                    <h5 >
-                                        <a>William Golding</a>
-                                    </h5>
-
-                                    <h5 style="font-size:1.5vw">
-                                        <b>Perpustakaan Cemerlang</b>
-                                    </h5>
-
-                                    <p class="mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur egestas, elit sit amet suscipit ultrices, justo velit sollicitudin nibh, et semper mi libero vitae turpis. Integer ornare scelerisque magna, et eleifend metus lobortis nec. Pellentesque turpis sapien, volutpat sit amet nibh eget, consequat pharetra mi. Ut luctus feugiat pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum accumsan nisl nec diam imperdiet, eu vehicula nibh pharetra. Nunc egestas lectus nunc, sed rhoncus leo egestas in. Maecenas massa augue, laoreet a elit id, consequat consectetur diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla posuere, massa eu fringilla suscipit, neque urna vulputate libero, non lobortis mauris dui non augue. Etiam vitae commodo orci, id gravida quam. Donec maximus molestie turpis sed dapibus. Nam sit amet porta dui, eget aliquet lorem. Nunc luctus ligula eget finibus placerat. Sed neque risus, ultricies sed neque faucibus, consectetur feugiat tortor.</p>
-
-                                </div>
-
+                                
                                 {{-- Button love https://codepen.io/jh3y/pen/eYZZdeK --}}
-
-                                <div class="mt-5 text-center">
-                                    <x-button class="">
-                                        Add to Cart
-                                    </x-button>
-                                    </div> 
-                                </div> 
                             </div>
                         </div>
                         
