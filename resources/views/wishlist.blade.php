446345bb221679b288961eb03f343bab8611c201
RifaJames <mrifajames@gmail.com>
2022-05-07 14:46:57 +0700

    on progress

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -24,15 +24,18 @@
 
                                 <div class="media-body">
                                     <h6 class="media-title font-weight-semibold">
-                                        <a href="#" data-abc="true">Apple iPhone XR (Red, 128 GB)</a>
+                                        <a href="#" data-abc="true">Lord of The Flies</a>
                                     </h6>
 
-                                    <ul class="list-inline list-inline-dotted mb-3 mb-lg-2">
-                                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Phones</a></li>
-                                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Mobiles</a></li>
-                                    </ul>
+                                    <h5 >
+                                        <a>William Golding</a>
+                                    </h5>
 
-                                    <p class="mb-3">128 GB ROM | 15.49 cm (6.1 inch) Display 12MP Rear Camera | 7MP Front Camera A12 Bionic Chip Processor | Gorilla Glass with high quality display </p>
+                                    <h5 >
+                                        <a>Perpustakaan Cemerlang</a>
+                                    </h5>
+
+                                    <p class="mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur egestas, elit sit amet suscipit ultrices, justo velit sollicitudin nibh, et semper mi libero vitae turpis. Integer ornare scelerisque magna, et eleifend metus lobortis nec. Pellentesque turpis sapien, volutpat sit amet nibh eget, consequat pharetra mi. Ut luctus feugiat pharetra. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum accumsan nisl nec diam imperdiet, eu vehicula nibh pharetra. Nunc egestas lectus nunc, sed rhoncus leo egestas in. Maecenas massa augue, laoreet a elit id, consequat consectetur diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla posuere, massa eu fringilla suscipit, neque urna vulputate libero, non lobortis mauris dui non augue. Etiam vitae commodo orci, id gravida quam. Donec maximus molestie turpis sed dapibus. Nam sit amet porta dui, eget aliquet lorem. Nunc luctus ligula eget finibus placerat. Sed neque risus, ultricies sed neque faucibus, consectetur feugiat tortor.</p>
 
                                     <ul class="list-inline list-inline-dotted mb-0">
                                         <li class="list-inline-item">All items from <a href="#" data-abc="true">Mobile point</a></li>
@@ -40,175 +43,14 @@
                                     </ul>
                                 </div>
 
-                                <div class="mt-3 mt-lg-0 ml-lg-3 text-center">
-                                    <h3 class="mb-0 font-weight-semibold">$459.99</h3>
-
-                                    <div>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-
-                                    </div>
-
-                                    <div class="text-muted">1985 reviews</div>
+                                <div class="mt-3 mt-lg-0 ml-lg-3 text-right">
 
                                     <button type="button" class="btn btn-warning mt-4 text-white"><i class="icon-cart-add mr-2"></i> Add to cart</button>
                                 </div>
                             </div>
                         </div>
-
-
-
-                    
-
-
-
-
-
-                    <div class="card card-body mt-3">
-                            <div class="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
-                                <div class="mr-2 mb-3 mb-lg-0">
-                                    
-                                        <img src="https://i.imgur.com/Aj0L4Wa.jpg" width="150" height="150" alt="">
-                                   
-                                </div>
-
-                                <div class="media-body">
-                                    <h6 class="media-title font-weight-semibold">
-                                        <a href="#" data-abc="true">Apple iPhone XS Max (Gold, 64 GB)</a>
-                                    </h6>
-
-                                    <ul class="list-inline list-inline-dotted mb-3 mb-lg-2">
-                                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Phones</a></li>
-                                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Mobiles</a></li>
-                                    </ul>
-
-                                    <p class="mb-3">256 GB ROM | 15.49 cm (6.1 inch) Display 12MP Rear Camera | 15MP Front Camera A12 Bionic Chip Processor | Gorilla Glass with high quality display </p>
-
-                                    <ul class="list-inline list-inline-dotted mb-0">
-                                        <li class="list-inline-item">All items from <a href="#" data-abc="true">Mobile junction</a></li>
-                                        <li class="list-inline-item">Add to <a href="#" data-abc="true">wishlist</a></li>
-                                    </ul>
-                                </div>
-
-                                <div class="mt-3 mt-lg-0 ml-lg-3 text-center">
-                                    <h3 class="mb-0 font-weight-semibold">$612.99</h3>
-
-                                    <div>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-
-                                    </div>
-
-                                    <div class="text-muted">2349 reviews</div>
-
-                                    <button type="button" class="btn btn-warning mt-4 text-white"><i class="icon-cart-add mr-2"></i> Add to cart</button>
-                                </div>
-                            </div>
-                        </div>    
-
-
-
-
-
-                        <div class="card card-body mt-3">
-                            <div class="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
-                                <div class="mr-2 mb-3 mb-lg-0">
-                                    
-                                        <img src="https://i.imgur.com/5Aqgz7o.jpg" width="150" height="150" alt="">
-                                   
-                                </div>
-
-                                <div class="media-body">
-                                    <h6 class="media-title font-weight-semibold">
-                                        <a href="#" data-abc="true">Apple iPhone XR (Red, 128 GB)</a>
-                                    </h6>
-
-                                    <ul class="list-inline list-inline-dotted mb-3 mb-lg-2">
-                                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Phones</a></li>
-                                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Mobiles</a></li>
-                                    </ul>
-
-                                    <p class="mb-3">128 GB ROM | 15.49 cm (6.1 inch) Display 12MP Rear Camera | 7MP Front Camera A12 Bionic Chip Processor | Gorilla Glass with high quality display </p>
-
-                                    <ul class="list-inline list-inline-dotted mb-0">
-                                        <li class="list-inline-item">All items from <a href="#" data-abc="true">Mobile point</a></li>
-                                        <li class="list-inline-item">Add to <a href="#" data-abc="true">wishlist</a></li>
-                                    </ul>
-                                </div>
-
-                                <div class="mt-3 mt-lg-0 ml-lg-3 text-center">
-                                    <h3 class="mb-0 font-weight-semibold">$459.99</h3>
-
-                                    <div>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-
-                                    </div>
-
-                                    <div class="text-muted">1985 reviews</div>
-
-                                    <button type="button" class="btn btn-warning mt-4 text-white"><i class="icon-cart-add mr-2"></i> Add to cart</button>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                         <div class="card card-body mt-3">
-                            <div class="media align-items-center align-items-lg-start text-center text-lg-left flex-column flex-lg-row">
-                                <div class="mr-2 mb-3 mb-lg-0">
-                                    
-                                        <img src="https://i.imgur.com/Aj0L4Wa.jpg" width="150" height="150" alt="">
-                                   
-                                </div>
-
-                                <div class="media-body">
-                                    <h6 class="media-title font-weight-semibold">
-                                        <a href="#" data-abc="true">Apple iPhone XS Max (Gold, 64 GB)</a>
-                                    </h6>
-
-                                    <ul class="list-inline list-inline-dotted mb-3 mb-lg-2">
-                                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Phones</a></li>
-                                        <li class="list-inline-item"><a href="#" class="text-muted" data-abc="true">Mobiles</a></li>
-                                    </ul>
-
-                                    <p class="mb-3">256 GB ROM | 15.49 cm (6.1 inch) Display 12MP Rear Camera | 15MP Front Camera A12 Bionic Chip Processor | Gorilla Glass with high quality display </p>
-
-                                    <ul class="list-inline list-inline-dotted mb-0">
-                                        <li class="list-inline-item">All items from <a href="#" data-abc="true">Mobile junction</a></li>
-                                        <li class="list-inline-item">Add to <a href="#" data-abc="true">wishlist</a></li>
-                                    </ul>
-                                </div>
-
-                                <div class="mt-3 mt-lg-0 ml-lg-3 text-center">
-                                    <h3 class="mb-0 font-weight-semibold">$612.99</h3>
-
-                                    <div>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-                                        <i class="fa fa-star"></i>
-
-                                    </div>
-
-                                    <div class="text-muted">2349 reviews</div>
-
-                                    <button type="button" class="btn btn-warning mt-4 text-white"><i class="icon-cart-add mr-2"></i> Add to cart</button>
-                                </div>
-                            </div>
-                        </div> 
                         
-
-
-                             
+                       
         </div>                     
         </div>
     </div>
