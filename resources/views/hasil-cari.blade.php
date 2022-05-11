8883d88d17b949bfb7e05c8049fbb2c33837db9e
Waran Singh <72307375+wsd15@users.noreply.github.com>
2022-05-11 22:59:53 +0700

    card buku

--- resources/views/hasil-cari.blade.php
+++ resources/views/hasil-cari.blade.php
@@ -6,39 +6,122 @@
     <body style="">
 
         <div class="container">
-            <div class="row">
-                <div class="col">
-                  <div class="card" style="width: 18rem;">
-                    <img src="https://source.unsplash.com/random/150x150?person" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="row mt-5 row-cols-3">
+                <div class="col mt-5 d-flex align-items-stretch">
+                    <div class="card" style="width: 17rem;">
+                        <img class="mt-4" src="https://source.unsplash.com/random/150x150?person"
+                            style="max-width: 65%;align-self: center" class="card-img-top" alt="...">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title text-center">Lord Of the Flies Lord Of the Flies</h5>
+                            <p class="card-text text-center">William Golding</p>
+                            <p class="card-text text-center mb-4">Jakarta Utara</p>
+                            <div class="d-flex justify-content-center" style="margin-top: auto">
+                                <x-button class=" align-self-end" style="align-self: center;">
+                                    {{ __('Detail Buku') }}
+                                </x-button>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-                <div class="col">
-                  <div class="card" style="width: 18rem;">
-                    <img src="https://source.unsplash.com/random/200x200?person" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="col mt-5 d-flex align-items-stretch">
+                    <div class="card" style="width: 17rem;">
+                        <img class="mt-4" src="https://source.unsplash.com/random/150x150?person"
+                            style="max-width: 65%;align-self: center" class="card-img-top" alt="...">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title text-center">Lord Of the Flies</h5>
+                            <p class="card-text text-center">William Golding</p>
+                            <p class="card-text text-center mb-4">Jakarta Utara</p>
+                            <div class="d-flex justify-content-center" style="margin-top: auto">
+                                <x-button class=" align-self-end" style="align-self: center;">
+                                    {{ __('Detail Buku') }}
+                                </x-button>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-                <div class="col">
-                  <div class="card" style="width: 18rem;">
-                    <img src="https://source.unsplash.com/random/150x150?person" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="col mt-5 d-flex align-items-stretch">
+                    <div class="card" style="width: 17rem;">
+                        <img class="mt-4" src="https://source.unsplash.com/random/150x150?person"
+                            style="max-width: 65%;align-self: center" class="card-img-top" alt="...">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title text-center">Lord Of the Flies</h5>
+                            <p class="card-text text-center">William Golding</p>
+                            <p class="card-text text-center mb-4">Jakarta Utara</p>
+                            <div class="d-flex justify-content-center" style="margin-top: auto">
+                                <x-button class=" align-self-end" style="align-self: center;">
+                                    {{ __('Detail Buku') }}
+                                </x-button>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              {{-- buat row 2-3-4-5-6 --}}
+                <div class="col mt-5 d-flex align-items-stretch">
+                  <div class="card" style="width: 17rem;">
+                      <img class="mt-4" src="https://source.unsplash.com/random/150x150?person"
+                          style="max-width: 65%;align-self: center" class="card-img-top" alt="...">
+                      <div class="card-body d-flex flex-column">
+                          <h5 class="card-title text-center">Lord Of the Flies</h5>
+                          <p class="card-text text-center">William Golding</p>
+                          <p class="card-text text-center mb-4">Jakarta Utara</p>
+                          <div class="d-flex justify-content-center" style="margin-top: auto">
+                              <x-button class=" align-self-end" style="align-self: center;">
+                                  {{ __('Detail Buku') }}
+                              </x-button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col mt-5 d-flex align-items-stretch">
+                <div class="card" style="width: 17rem;">
+                    <img class="mt-4" src="https://source.unsplash.com/random/150x150?person"
+                        style="max-width: 65%;align-self: center" class="card-img-top" alt="...">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title text-center">Lord Of the Flies</h5>
+                        <p class="card-text text-center">William Golding</p>
+                        <p class="card-text text-center mb-4">Jakarta Utara</p>
+                        <div class="d-flex justify-content-center" style="margin-top: auto">
+                            <x-button class=" align-self-end" style="align-self: center;">
+                                {{ __('Detail Buku') }}
+                            </x-button>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div class="col mt-5 d-flex align-items-stretch">
+              <div class="card" style="width: 17rem;">
+                  <img class="mt-4" src="https://source.unsplash.com/random/150x150?person"
+                      style="max-width: 65%;align-self: center" class="card-img-top" alt="...">
+                  <div class="card-body d-flex flex-column">
+                      <h5 class="card-title text-center">Lord Of the Flies</h5>
+                      <p class="card-text text-center">William Golding</p>
+                      <p class="card-text text-center mb-4">Jakarta Utara</p>
+                      <div class="d-flex justify-content-center" style="margin-top: auto">
+                          <x-button class=" align-self-end" style="align-self: center;">
+                              {{ __('Detail Buku') }}
+                          </x-button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col mt-5 d-flex align-items-stretch">
+            <div class="card" style="width: 17rem;">
+                <img class="mt-4" src="https://source.unsplash.com/random/150x150?person"
+                    style="max-width: 65%;align-self: center" class="card-img-top" alt="...">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title text-center">Lord Of the Flies</h5>
+                    <p class="card-text text-center">William Golding</p>
+                    <p class="card-text text-center mb-4">Jakarta Utara</p>
+                    <div class="d-flex justify-content-center" style="margin-top: auto">
+                        <x-button class=" align-self-end" style="align-self: center;">
+                            {{ __('Detail Buku') }}
+                        </x-button>
+                    </div>
+                </div>
+            </div>
+        </div>
+                
+            </div>
+            {{-- buat row 2-3-4-5-6 --}}
         </div>
 
 
