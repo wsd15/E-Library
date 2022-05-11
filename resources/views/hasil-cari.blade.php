bbf5eb60e0f36428adf5f9559143fa731b8f07a9
Waran Singh <72307375+wsd15@users.noreply.github.com>
2022-05-11 22:13:53 +0700

    card

---
+++ resources/views/hasil-cari.blade.php
@@ -0,0 +1,49 @@
+@extends('layout.app')
+
+@section('content')
+    <html>
+
+    <body style="">
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                  <div class="card" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/random/150x150?person" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/random/200x200?person" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/random/150x150?person" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+              {{-- buat row 2-3-4-5-6 --}}
+            </div>
+        </div>
+
+
+
+    </body>
+
+    </html>
+@endsection
