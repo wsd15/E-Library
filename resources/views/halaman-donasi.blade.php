d29e86489368be9b39f7aa833953db8fe12fb4bc
Waran Singh <72307375+wsd15@users.noreply.github.com>
2022-05-12 10:50:11 +0700

    done halaman donasi

--- resources/views/halaman-donasi.blade.php
+++ resources/views/halaman-donasi.blade.php
@@ -3,25 +3,25 @@
 @section('content')
     <html>
 
-    <body style="">
+    <body style="min-height: 0px">
         <div id="bg">
             <img src="images/donasi.png" alt="">
         </div>
 
-        <div class="container border border-4 border-dark mt-5 bg-white bg-opacity-50" style="border-radius: 30px;">
+        <div class="container border border-4 border-dark mt-5 bg-white bg-opacity-50" style="border-radius: 30px;max-width: 700px">
             <br>
             <p class="text-center fw-bolder">Terimakasih Sudah Berpartisipasi Untuk Berdonasi</p>
             <p class="text-center fw-bolder">Silahkan Hubungi Kami dan Atur Pengiriman</p>
             <br>
-            <p class="fw-bolder">WhatsApp : </p> <a class="fw-bolder" style="color: blue"
+            <p class="fw-bolder ml-4">WhatsApp : </p> <a class="fw-bolder ml-4" style="color: blue"
                 href="wa.me/6281210404587">wa.me/6281210404587</a>
             <br>
             <br>
-            <p class="fw-bolder">Alamat Perpustakaan : </p>
-            <p class="fw-bolder">Jl. Swadaya Barat RT 015/RW 05 No. 50A</p>
+            <p class="fw-bolder ml-4">Alamat Perpustakaan : </p>
+            <p class="fw-bolder ml-4">Jl. Swadaya Barat RT 015/RW 05 No. 50A</p>
             <br>
-            <p class="fw-bolder">Google Maps : </p>
-            <a class="fw-bolder" style="color: blue"
+            <p class="fw-bolder ml-4">Google Maps : </p>
+            <a class="fw-bolder ml-4" style="color: blue"
                 href="https://goo.gl/maps/ukTx6gduD37nqArK8">https://goo.gl/maps/ukTx6gduD37nqArK8</a>
             <br>
             <br>
