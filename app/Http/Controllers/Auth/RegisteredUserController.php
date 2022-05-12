8c39964865e45185bf93ef7d3aaf7198d8503e2d
Waran Singh <72307375+wsd15@users.noreply.github.com>
2022-05-12 15:42:41 +0700

    done roles

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,7 +48,7 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-
+        $user->attachRole('user');
         return redirect(RouteServiceProvider::HOME);
     }
 }
